c8a8548e60b6226f8428d60f42336032e4998bbd
Дмитрий Бурмистров <supamem74@gmail.com>
2022-12-11 23:05:26 +0300

    init-react-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import './App.scss';
+import { Link, Outlet } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      @@@@sss
+      <header>
+        <nav className='navbar'>
+          <Link className='navbar__link' to='/' > Home </Link>
+          <Link className='navbar__link' to='/main' > Main </Link>
+        </nav>
+      </header>
+      <div className='router-container'>
+        <Outlet/>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
+import MainView from './views/MainView/MainView.js'
+import ErrorView from './views/ErrorView'
+import HomeView from './views/HomeView/HomeView'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorView/>,
+    children: [
+      {
+        path: "/",
+        element: <HomeView/>
+      },
+      {
+        path: "/main",
+        element: <MainView/>,
+      },
+    ]
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
