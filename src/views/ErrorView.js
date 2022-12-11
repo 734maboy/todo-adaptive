ecb67ec83998bc626c339c897e42b2e394eaa2d9
Дмитрий Бурмистров <supamem74@gmail.com>
2022-12-11 23:05:49 +0300

    feature-error-view

---
+++ src/views/ErrorView.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../style/error-view.scss'
+
+const ErrorView = () => {
+	return (
+		<div className='error-container'>
+			<h1>Whoops..</h1>
+			<span>Something went wrong</span>
+		</div>
+	)
+}
+
+export default ErrorView
